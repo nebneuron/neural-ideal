ca9eeb0544431295c1cbf19cfa37735e54f7b86f
Nora <nora.youngs@gmail.com>
2015-07-30 13:36:58 -0700

    Update Prettify.m

--- Prettify.m
+++ Prettify.m
@@ -1,38 +1,39 @@
-function[D] = Prettify(C,sigma)
+function[D] = Prettify(C)
 
-% Given an ideal in matrix form C and (optionally) a set of indices,
-%Prettify returns the set of polynomials which correspond to that matrix
-%written as strings.  sigma gives the appropriate set of indices so sigma
-%should have the same length as there are columns in C.
+% Given a canonical form in matrix form C
+%where the first half of the digits are x_i's and
+%the second half are 1-x_i's,
+%this now returns the set of polynomials which correspond to that matrix
+%written as strings.  
 
 %INPUT
-% C = a ternary matrix representing a pseudo-monomial idea
-% sigma = a set of indices we want the matrix to correspond to.
+% C = a binary matrix representing a pseudo-monomial ideal.
 
 % OUTPUT
 % D = a list of strings giving the pseudo-monomials that C encodes.
 
 c=size(C);
+d=c(2)/2;
 
-if nargin < 2,
-    sigma = 1:c(2);  % set the default sigma to be [1,...,n]
-end
+%if nargin < 2,
+%    sigma = 1:c(2);  % set the default sigma to be [1,...,n]
+% end
 
 
 D=[];
-for s=1:c(1)   % for each row
-    k=C(s,:);         
+for s=1:c(1)   % for each row         
     var=[];
-    M=find(k==1);   % find the 1's
-    m=size(M); 
-    N=find(k==0);  % find the 0's
-    for j=1:m(2),                                %write the 1s as 1-x_i
-          var = [var,' (1-x',num2str(sigma(M(j))),')'];  % with the appropriate index from sigma
+    for j=1:d,  %consider the first half
+        if C(s,j)==1   %write 1's  as corresponding x_i
+          var = [var,'x',num2str(j)];  
+        end
+        
     end
     
-    n=size(N);
-    for i=1:n(2)                                    % write the 0s as x_i
-	  var = [var, ' x',num2str(sigma(N(i)))]; % with the appropriate index from sigma
+    for i=d+1:c(2)      %consider the second half
+        if C(s,i)==1    %write 1's as corresponiding (1-x_i)
+            var = [var, '(1-x',num2str(i-d),')'];
+        end                                 
     end
     D=char(D,var);
 end
