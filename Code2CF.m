0255f95cacdd2e674cf0b47ee24cb3a619679585
Nora <nora.youngs@gmail.com>
2015-07-30 13:34:36 -0700

    Update Code2CF.m

--- Code2CF.m
+++ Code2CF.m
@@ -1,23 +1,45 @@
-function [CF,t] = Code2CF(C)
+function [CF,MAT] = Code2CF(C)
 %----------------------------------------------------------------------
 %Loops through intersecting ideals until we have the canonical form.
+%
+%INPUT: 
+%C: a binary matrix whose rows are codewords, of length n
 
-%C: set of codewords, number of codewords by number of neurons
+%OUTPUT:
+%CF: canonical form  in pseudo-monomial format (Automatic)
 
-%CF: canonical form represented by 0's (xi) 1's (1-xi) and 5's (nothing)
-
+%MAT: the matrix form storing canonical form information. (Optional)
+%       Each row represents a pseudo-monomial;  1 in the ith position 
+%       (for i <= n) indicates x_i is a factor; 1 in the ith position 
+%       (for i>n) indicates (1-x_{i-n}) is a factor.
+%
 %t: the number of operations it performed, used for time complexity
-%analysis
+%analysis (Not Currently Used)
 
 L=diag(C(1,:))+ones(size(C,2),size(C,2))*5-eye(size(C,2))*5;
 [I,t]=FinalIntersectIdeals(L,C(2,:));
 
-for i=3:size(C,1)
+for i=3:size(C,1)  
     r=C(i,:);
-    [I,t0]=FinalIntersectIdeals(I,r);
+    [I,t0]=FinalIntersectIdeals(I,r); %compute repeated intersections
     t=t+t0;
 end
-CF=I;
 
-                
+[a,b]=size(I);  % Here we rewrite the final information to
+M=zeros(a,2*b); % an easier-to-read matrix format 
+for i=1:a
+    for j=1:b
+        if I(i,j)==1
+            M(i,j+b)=1;
+        elseif I(i,j)==0
+            M(i,j)=1;
+        end
+    end
+end
+    
+
+CF=Prettify(M);
+MAT=M;
+
+
 
